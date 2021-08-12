c08af9b6607b9ae989d414c3480c79cd01fcdfb1
Vector-7 <seokbong60@gmail.com>
2021-08-12 19:22:01 +0900

    init file control test script

--- app/server/app/tests/test_file_dir_control.py
+++ app/server/app/tests/test_file_dir_control.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 
 from module.data_builder.user_builder import UserBuilder
-from app.models import User
+from module.specification.System_config import SystemConfig
+import os
+import sys
 
 
 class FileDirControlTestUnittest(TestCase):
@@ -37,6 +39,15 @@ class FileDirControlTestUnittest(TestCase):
         DirectoryDataBuilder, DirectoryData
     """
 
+    # Variables
+    CONFIG_FILE_ROOT: str = ["server", "config.json"]
+    system_config: SystemConfig = SystemConfig(os.path.join(*CONFIG_FILE_ROOT))
+
+    # User 저장소
+    test_user_root: str = f"{system_config.get_system_root()}\\storage\\" if sys.platform == "win32" \
+        else f"{system_config.get_system_root()}/storage/"
+    cur_root: str = ""
+
     def setUp(self) -> None:
         user_builder = UserBuilder()
         user_builder.set_name("admin") \
@@ -47,8 +58,19 @@ class FileDirControlTestUnittest(TestCase):
             .set_static_id() \
             .build().save()
 
+        # User Root 갱신
+        self.test_user_root += user_builder.static_id
+        os.mkdir(self.test_user_root)
+
+        # 해당 User 에 대한 파일 및 디렉토리 관리는 root 에서 진행한다.
+        self.cur_root = f"{self.test_user_root}\\root" if sys.platform == "win32" else f"{self.test_user_root}/root"
+        os.mkdir(self.cur_root)
+
+        # Check is Directory Exist
+        self.assertEqual(os.path.isdir(self.cur_root), True)
+
     def test_make_new_directory(self):
-        # 디렉토리 생성 테스트
+        # 해당 User Directory 가 정상적으로 저장되었는 지 확인
         pass
 
     def test_remove_new_directory(self):
@@ -62,8 +84,15 @@ class FileDirControlTestUnittest(TestCase):
     def test_get_information_of_file(self):
         # 파일 관련 정보 갖고오기
         pass
-    
+
     def test_modify_file_name(self):
         # 파일 이름 바꾸기 테스트
         pass
 
+    def test_get_directory_info_and_file_list(self):
+        # 디렉토리 정보와 파일 조회하기
+        pass
+
+    def test_remove_directory_recursive(self):
+        # 디렉토리 순환 삭제
+        pass
