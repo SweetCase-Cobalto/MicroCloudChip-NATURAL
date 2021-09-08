836f4c16a53cc37ecd0b3e66b1827eef1225c1b3
Vector-7 <seokbong60@gmail.com>
2021-09-09 05:11:22 +0900

    product test setting

--- app/server/server/urls.py
+++ app/server/server/urls.py
@@ -26,8 +26,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
 
-    #url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    #url(r'^(storage)|(accounts)|(settings)/', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^(storage)|(accounts)|(settings)/', TemplateView.as_view(template_name='index.html'), name='index'),
 
 
     path('admin/', admin.site.urls),
