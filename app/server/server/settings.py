fbac387466a9078ea2c9c780d5a132e17db0373e
Vector-7 <seokbong60@gmail.com>
2021-10-04 04:43:55 +0900

    Chore: deploy production 0.0.4
    0.0.4 배포를 위한 setting, urls 코드 수정

--- app/server/server/settings.py
+++ app/server/server/settings.py
@@ -69,8 +69,8 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        #'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        #'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,11 +82,11 @@ TEMPLATES = [
         },
     },
 ]
-"""
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates', 'static')
 ]
-"""
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
@@ -165,7 +165,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
+"""
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
@@ -178,7 +178,7 @@ ALLOWED_HOSTS = [
     "0.0.0.0",
     "[::1]"
 ]
-"""
+
 
 CORS_ALLOW_HEADERS = [
     'Set-Cookie'
